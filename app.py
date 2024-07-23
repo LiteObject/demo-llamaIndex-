17fb2301aa019a5bd54783414c5f6fa970f53fad
LiteObject <Lite.Object@gmail.com>
2024-07-23 16:11:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,47 +10,57 @@ def console_print(message: str, color_name: str = Color.WHITE) -> None:
     print(color_name + message + Color.reset())
 
 
-# Create a spinner
-spinner = Halo(text='Loading', spinner='dots')
-
-try:
+def load_document(folder: str = ""):
+    if not folder:
+        raise ValueError("The 'folder' parameter cannot be null or empty.")
+    
     # Start the spinner
     spinner.start()
+
     # bge-base embedding model
     Settings.embed_model = HuggingFaceEmbedding(
         model_name="BAAI/bge-base-en-v1.5")
     Settings.llm = Ollama(model="llama3", request_timeout=360.0)
 
-     # Load all the text files from the specified "data" directory into a list of Document objects.
-    documents = SimpleDirectoryReader("data").load_data()
-    index = VectorStoreIndex.from_documents(documents)
-    query_engine = index.as_query_engine()
+    # Load all the text files from the specified "data" directory into a list of Document objects.
+    documents = SimpleDirectoryReader(folder).load_data()
+    index: VectorStoreIndex = VectorStoreIndex.from_documents(documents)
+    base_query_engine = index.as_query_engine()
     spinner.stop()
     console_print("The database is up to date.", Color.LIGHT_GRAY)
+    return base_query_engine
 
-    # response = query_engine.query("Who is Byte?")
-    # print(response)
-except (ValueError, TypeError) as e:
-    spinner.fail()
-    console_print(f"Task failed: {e}", Color.RED)
 
-while True:
-    # Get the question from the user
-    user_question = input(
-        Color.LIGHT_BLUE + "Please enter your question (or type 'quit' to exit):" + Color.reset() + "\n")
+if __name__ == "__main__":
+    # Create a spinner
+    spinner = Halo(text='Loading', spinner='dots')
 
-    # Check if the user wants to quit
-    if user_question.lower() in ['quit', 'qq', 'bye', 'exit']:
-        break
+    try:
 
-    spinner.start()
+        query_engine = load_document("data")
 
-    # Use the user's question in the query
-    response = query_engine.query(user_question)
+        while True:
+            # Get the question from the user
+            user_question = input(
+                Color.LIGHT_BLUE + "Please enter your question (or type 'quit' to exit):" + Color.reset() + "\n")
 
-    spinner.stop()
+            # Check if the user wants to quit
+            if user_question.lower() in ['quit', 'qq', 'bye', 'exit']:
+                break
 
-    # Print the response
-    console_print(str(response) + "\n", Color.CYAN)
+            spinner.start()
 
-console_print("Thank you for using the query engine!\n", Color.LIGHT_GRAY)
+            # Use the user's question in the query
+            response = query_engine.query(user_question)
+
+            spinner.stop()
+
+            # Print the response
+            console_print(str(response) + "\n", Color.CYAN)
+
+        console_print("Thank you for using the query engine!\n",
+                      Color.LIGHT_GRAY)
+
+    except (ValueError, TypeError) as e:
+        spinner.fail()
+        console_print(f"Task failed: {e}", Color.RED)
